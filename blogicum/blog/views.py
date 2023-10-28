15846df2e09713d0b943ce27b74a1af5b6e0df42
k3214891 <k3214891@yandex.ru>
2023-10-29 02:56:01 +0300

    v7

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,8 +1,9 @@
 from typing import Any
 from django.db import models
-from django.http import HttpRequest
+from django.http import HttpRequest, Http404
 # Подключите к проекту пагинацию
 from django.core.paginator import Paginator
+from django.utils import timezone
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
@@ -14,6 +15,7 @@ from .models import Post, Category, User, Comment
 from .forms import CommentForm, UserForm, PostForm
 
 POST_ID = 'post_id'
+COM_ID = 'comment_id'
 # Настройте вывод не более 10 публикаций (переменная для пагинатора)
 POSTS_LIMIT = 10
 
@@ -59,16 +61,17 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     form_class = PostForm
     template_name = 'blog/create.html'
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
 # No URL to redirect to.  Either provide a url or define a get_absolute_url method on the Model.
     def get_success_url(self):
         username = self.request.user.username
         return reverse_lazy('blog:profile', kwargs={'username': username})
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     pk_url_kwarg = POST_ID
     form_class = PostForm
@@ -85,12 +88,28 @@ class PostDeleteView(DeleteView):
         return reverse_lazy('blog:delete_post',
                             kwargs={'post_id': self.kwargs['post_id']})
     
-
-class PostUpdateView(UpdateView):
+# Убедитесь, что пользователь не может редактировать чужие посты
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     pk_url_kwarg = POST_ID
     form_class = PostForm
     template_name = 'blog/create.html'
+# PostUpdateView is missing the implementation of the test_func() method
+    def test_func(self):
+        if self.request.user != self.get_object().author:
+            return False
+        return True
+    
+#    def get_success_url(self):
+# На страницу обновленного поста
+# Убедитесь, что при отправке формы редактирования поста неавторизованным пользователем он перенаправляется на страницу публикации (/posts/<int:post_id>/)
+#        return reverse_lazy('blog:post_detail',
+#                            kwargs={'post_id': self.kwargs['post_id']})
+
+# Убедитесь, что пользователь не может редактировать чужие посты
+    def handle_no_permission(self):
+        return reverse_lazy('blog:post_detail',
+                            kwargs={'post_id': self.kwargs['post_id']})
 
 
 # The current path, accounts/profile/, didn’t match any of these
@@ -104,12 +123,12 @@ class ProfileDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         author = self.object
-        object_list = Post.post_objects.select_related()
+        object_list = Post.post_objects.filter(author=author)
 # Все посты автора
         if self.request.user == author:
-            object_list = Post.post_objects.select_related('author',)
+            object_list = Post.objects.all()
 # Пагинация
-        #object_list = object_list.filter(author=author)
+        # object_list = object_list.filter(author=author)
         context = super().get_context_data(**kwargs)
         page_num = self.request.GET.get('page', 1)
         paginator = Paginator(object_list, POSTS_LIMIT)
@@ -140,7 +159,11 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         form.instance.post = get_object_or_404(
-            Post.post_objects.select_related(),
+            Post.post_objects.filter(
+                is_published=True,
+                category__is_published=True,
+                pub_date__lte=timezone.now()
+            ),
             pk=self.kwargs[POST_ID]
         )
         return super().form_valid(form)
@@ -151,19 +174,41 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
 
 
 
-class CommentUpdateView(LoginRequiredMixin, UpdateView):
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    pk_url_kwarg = POST_ID
+    pk_url_kwarg = COM_ID
     form_class = PostForm
     template_name = 'blog/create.html'
 
+    def test_func(self):
+        if self.request.user != self.get_object().author:
+            return False
+        return True
+# Убедитесь, что автору комментария видна ссылка на страницу редактирования этого комментария. Проверьте, что в словарь контекста для страницы редактирования комментария передаётся объект формы
+    def handle_no_permission(self):
+        #raise Http404('Страница не найдена')
+        return reverse_lazy('blog:edit_comment',
+                            kwargs={'post_id': self.kwargs['post_id']})
 
-class CommentDeleteView(LoginRequiredMixin, DeleteView):
+# Есть вариант реализации через dispatch
+# Страницы удаления и редактирования комментария должны иметь идентичные права доступа. Убедитесь, что GET-запрос к этим страницам возвращает один и тот же статус и не удаляет комментарий
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    pk_url_kwarg = POST_ID
+    pk_url_kwarg = COM_ID
     form_class = PostForm
     template_name = 'blog/create.html'
 
+# Страницы удаления и редактирования комментария должны иметь идентичные права доступа. Убедитесь, что GET-запрос к этим страницам возвращает один и тот же статус и не удаляет комментарий
+# Относится к UserPassesTestMixin
+    def test_func(self):
+        if self.request.user != self.get_object().author:
+            return False
+        return True
+
+    def handle_no_permission(self):
+        #raise Http404('Страница не найдена')
+        return reverse_lazy('blog:delete_comment',
+                            kwargs={'post_id': self.kwargs['post_id']})
 
 class CategoryPostListView(ListView):
     paginate_by = POSTS_LIMIT
